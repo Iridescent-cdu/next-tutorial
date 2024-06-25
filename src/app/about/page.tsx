c76401df2bfad70bf379252d023ab8b7c573a8f0
iridescent <2389504513@qq.com>
2024-06-25 23:27:01 +0800

    ✨ feat: 测试Image组件

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,12 @@
 import styles from "./page.module.css";
+import Image from "next/image";
+import NextSvg from "../../../public/next.svg";
+import DouyinPng from "../../../public/douyin.png";
 
 export default function AboutPage() {
-  return <h1 className={styles.main}>About Us</h1>
+  return <>
+    <h1 className={styles.main}>About Us</h1>
+    <Image src={NextSvg} alt={""}></Image>
+    <Image src={DouyinPng} alt={""}></Image>
+  </>
 }
