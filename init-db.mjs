60e9e554f6d5bb802fb0c04f7696f45a1167b027
iridescent <2389504513@qq.com>
2024-06-27 00:48:49 +0800

    ✨ feat: 使用sqllite测试请求

---
+++ init-db.mjs
@@ -0,0 +1,39 @@
+import sql from 'better-sqlite3'
+
+const db = sql('meal.db')
+
+db.prepare(`
+    CREATE TABLE IF NOT EXISTS meals (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      slug TEXT NOT NULL UNIQUE,
+      title TEXT NOT NULL,
+      image TEXT NOT NULL,
+      summary TEXT NOT NULL,
+      instructions TEXT NOT NULL,
+      creator TEXT NOT NULL,
+      creator_email TEXT NOT NULL
+   )
+`).run();
+
+
+const stmt = db.prepare(`
+   INSERT INTO meals (slug, title, image, summary, instructions, creator, creator_email)
+   VALUES (@slug, @title, @image, @summary, @instructions, @creator, @creator_email)
+`);
+
+const dummyMeals = [
+  {
+    slug: 'chicken-curry',
+    title: 'Chicken Curry',
+    image: 'https://www.themealdb.com/images/media/meals/xrsqqx1511637373.jpg',
+    summary: 'Chicken curry is a type of curry that is made with chicken as its primary ingredient.',
+    instructions: 'In a large skillet, heat2 tablespoons of oil over medium heat. Add the chopped onion and cook until',
+    creator: 'Max Schwarz',
+    creator_email: 'max@example.com',
+  }
+]
+
+for (const meal of dummyMeals) {
+  stmt.run(meal)
+}
+
--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,9 @@
+import { getMeals } from "./db";
 import styles from "./page.module.css";
 
-export default function BlogPost({ params }) {
+export default async function BlogPost({ params }) {
+  await getMeals()
+
   return <main className={styles.main}>
     <h1>Blog Post</h1>
     <p>{params.slug}</p>
