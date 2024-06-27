e2a337fe7bb3391597dde167a27358f3455ea247
iridescent <2389504513@qq.com>
2024-06-27 21:39:47 +0800

    ✨ feat: 使用suspense代理loading处理局部加载效果

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,11 +1,20 @@
+import { Suspense } from "react";
 import { getMeals } from "./db";
 import styles from "./page.module.css";
 
-export default async function BlogPost({ params }) {
+async function Post() {
   await getMeals()
+  return <div>loading out</div>
+}
+
+export default function BlogPost({ params }) {
+
 
   return <main className={styles.main}>
     <h1>Blog Post</h1>
     <p>{params.slug}</p>
+    <Suspense fallback={<div>loading</div>}>
+      <Post />
+    </Suspense>
   </main>
 }
