b1e51d9ec607a39dd40895ba246e2a9dc263ccd1
iridescent <2389504513@qq.com>
2024-07-13 15:24:48 +0800

    ✨ feat(auth): 添加CSRF校验

--- examples/app-router/src/middleware.ts
+++ examples/app-router/src/middleware.ts
@@ -1,7 +1,17 @@
+import { verifyRequestOrigin } from 'lucia';
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-
+  if (request.method === 'GET') {
+    return NextResponse.next()
+  }
+  const originHeader = request.headers.get('origin')
+  const hostHeader = request.headers.get('host')
+  if (!originHeader || !hostHeader || verifyRequestOrigin(originHeader, [hostHeader])) {
+    return new NextResponse(null, {
+      status: 403
+    })
+  }
   return NextResponse.next()
 }
 
