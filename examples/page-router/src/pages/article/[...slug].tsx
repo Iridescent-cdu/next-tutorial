2e982582f95633c3d614fab553fc1cf3863a1102
iridescent <2389504513@qq.com>
2024-07-13 23:12:06 +0800

    ✨ feat(query): 测试路由参数获取

--- examples/page-router/src/pages/article/[...slug].tsx
+++ examples/page-router/src/pages/article/[...slug].tsx
@@ -5,7 +5,10 @@ type Props = {}
 const ArticlePage = (props: Props) => {
   const router = useRouter()
 
-  /** SSG 的构建过程是在没有实际用户请求的情况下完成的，因此无法获取动态路由参数，默认为 {} */
+  /** 
+   * 服务端下： SSG 的构建过程是在没有实际用户请求的情况下完成的，因此无法获取动态路由参数，默认为 {}
+   * 客户端下： 可以正确获取到路由路径参数
+   */
   console.log(router.query)
 
   return (
@@ -13,7 +16,7 @@ const ArticlePage = (props: Props) => {
   )
 }
 
-/** 显示指定为SSR后，可以获取到路由参数 */
+/** 显示指定为SSR后，服务端可以获取到路由参数 */
 export const getServerSideProps = async (context) => {
   return {
     props: {}
