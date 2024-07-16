803abf4f3e61df2f8efd662908038d372b4c9e7f
iridescent <2389504513@qq.com>
2024-07-16 13:45:16 +0800

    ✨ feat(page-router): 添加Layout、Head

--- examples/page-router/src/pages/_app.tsx
+++ examples/page-router/src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Layout>
+    <Head>
+      <title>Page Router</title>
+      <meta property="og:title" content="My page title" key="title" />
+    </Head>
+    <Component {...pageProps} />
+  </Layout>;
 }
